b9b7b1b903fdc53a16ddba1a9581c60ffbed1fae
Othmane Nouakchi <onouakch@e2r3p10.1337.ma>
2022-10-08 14:03:08 +0100

    commit

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -16,21 +16,12 @@ void *ft_memmove(void *dst, const void *src, size_t len)
 {
     size_t i;
 
-    i = 0;
+    i = -1;
     if (dst < src)
-        while (i < len)
-        {
+        while (++i < len)
             ((char *)dst)[i] = ((char *)src)[i];
-            i++;
-        }
     else
-    {
-        i = len - 1;
-        while (i >= 0)
-        {
-            ((char *)dst)[i] = ((char *)src)[i];
-            i--;
-        }
-    }
+        while (--len >= 0)
+            ((char *)dst)[len] = ((char *)src)[len];
     return (dst);
 }
