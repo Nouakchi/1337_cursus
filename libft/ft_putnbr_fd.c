008d70bc059e06d5b7abaaca0c46080a6df5d288
Othmane Nouakchi <onouakch@e1r9p14.1337.ma>
2022-10-09 17:35:19 +0100

    commit

---
+++ libft/ft_putnbr_fd.c
@@ -0,0 +1,29 @@
+
+
+#include "libft.h"
+
+void ft_putnbr_fd(int n, int fd)
+{
+    if (n == -2147483648)
+    {
+        n = 147483648;
+        ft_putstr_fd("-2", fd);
+    }
+    if (n < 0)
+    {
+        n *= -1;
+        ft_putchar_fd('-', fd);
+    }
+    if (n >= 10)
+    {
+        ft_putnbr_fd(n / 10, fd);
+        ft_putnbr_fd(n % 10, fd);
+    }
+    else
+        ft_putchar_fd(n + 48, fd);
+}
+
+int main()
+{
+    ft_putnbr_fd(54351, 1);
+}
--- libft/main.c
+++
@@ -1,8 +0,0 @@
-#include "libft.h"
-
-int main () {
-  char str[] = "holidays";
-  char tof[] = "hol";
-  printf("%s", ft_strtrim(str, tof));
-  return (0);
-}
