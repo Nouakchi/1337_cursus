8a50e305a293a752f06443396a69340aa79c70f6
Othmane Nouakchi <onouakch@e1r4p9.1337.ma>
2022-10-21 21:04:57 +0100

    commit

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -6,17 +6,20 @@
 /*   By: onouakch <onouakch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 21:51:46 by onouakch          #+#    #+#             */
-/*   Updated: 2022/10/17 20:46:10 by onouakch         ###   ########.fr       */
+/*   Updated: 2022/10/21 21:03:44 by onouakch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+void del(void *lst)
+{
+	(void)lst;
+}
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
 	if (lst && del)
 	{
-		del(lst->content);
+		del(lst);
 		free(lst);
 	}
 }
--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,13 +6,11 @@
 /*   By: onouakch <onouakch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 16:16:52 by onouakch          #+#    #+#             */
-/*   Updated: 2022/10/21 01:02:32 by onouakch         ###   ########.fr       */
+/*   Updated: 2022/10/21 04:13:12 by onouakch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-#include <stdio.h>
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
