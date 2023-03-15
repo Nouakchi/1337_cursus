df14b8b3acb6d57d70ce65f7d1d479255095a0c8
Nouakchi <Othman.nouakchi007@gmail.com>
2023-03-15 20:35:01 +0100

    threads init

--- philosophers/includes/philosophers.h
+++ philosophers/includes/philosophers.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: onouakch <onouakch@student.42.fr>          +#+  +:+       +#+        */
+/*   By: othman <othman@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 11:11:39 by onouakch          #+#    #+#             */
-/*   Updated: 2023/03/13 11:56:03 by onouakch         ###   ########.fr       */
+/*   Updated: 2023/03/15 20:32:31 by othman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,41 @@
 # define PHILO_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+typedef struct s_philo
+{
+    pthread_t   philo;
+    int index;
+    int activity;
+    int alive;
+}   t_philo;
+
+typedef struct s_fork
+{
+    int status;
+}   t_fork;
+
+typedef struct s_vars
+{
+    pthread_mutex_t mutex;
+    t_philo   *philos;
+    t_fork     *forks;
+    int current_philo;
+    int nbr_philo;
+    int t_die;
+    int t_eat;
+    int t_sleep;
+    int nbr_philo_eat;
+}   t_vars;
+
+typedef struct s_data
+{
+    t_philo philo;
+    t_vars  *vars;
+}   t_data;
+
 
 #endif
--- philosophers/philo/main.c
+++ philosophers/philo/main.c
@@ -3,17 +3,72 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: onouakch <onouakch@student.42.fr>          +#+  +:+       +#+        */
+/*   By: othman <othman@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 11:12:29 by onouakch          #+#    #+#             */
-/*   Updated: 2023/03/13 11:30:27 by onouakch         ###   ########.fr       */
+/*   Updated: 2023/03/15 20:33:58 by othman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
+void    *live(void *vars)
+{
+    t_data  *args;
+    
+    args = vars;
+    printf("%d\n", args->philo.index);
+    // while (1)
+    // {
+    //     if ()
+    // }
+    
+    return (NULL);
+}
+
+void    ft_philos_init(t_vars *vars)
+{
+    int i;
+
+    i = -1;
+    vars->philos = malloc(sizeof(t_philo) * vars->nbr_philo);
+    vars->forks = malloc(sizeof(t_fork) * vars->nbr_philo);
+    while (++i < vars->nbr_philo)
+    {
+        vars->forks[i].status = 0;
+        vars->philos[i].index = i + 1;
+        vars->philos[i].alive = 1;
+        vars->philos[i].activity = 1;
+        vars->current_philo = i;
+        pthread_create(&vars->philos[i].philo, NULL, &live
+                , &(t_data){vars->philos[i], vars});
+        usleep(50);
+    }
+}
+
+void    ft_philos_join(t_vars *vars)
+{
+    int i;
+
+    i = -1;
+    while (++i < vars->nbr_philo)
+        pthread_join(vars->philos[i].philo, NULL);
+}
+
+void    philo_loop(int nbr_philo)
+{
+    t_vars  vars;
+    
+    vars.nbr_philo = nbr_philo;
+    ft_philos_init(&vars);
+    ft_philos_join(&vars);
+}
+
 int main(int ac, char *av[])
 {
-    printf("%d | %s\n", ac , av[0]);
+    (void)ac;
+    (void)av;
+    
+    philo_loop(4);
     return (0);
 }
