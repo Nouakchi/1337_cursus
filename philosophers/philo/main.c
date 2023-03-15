8851d0627ccdc701bd912ed072e087a705175dff
Nouakchi <Othman.nouakchi007@gmail.com>
2023-03-15 21:30:15 +0100

    let the philosophers to start theire activities

--- philosophers/philo/main.c
+++ philosophers/philo/main.c
@@ -6,7 +6,7 @@
 /*   By: othman <othman@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 11:12:29 by onouakch          #+#    #+#             */
-/*   Updated: 2023/03/15 20:33:58 by othman           ###   ########.fr       */
+/*   Updated: 2023/03/15 21:27:27 by othman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,112 @@ void    *live(void *vars)
     t_data  *args;
     
     args = vars;
-    printf("%d\n", args->philo.index);
-    // while (1)
-    // {
-    //     if ()
-    // }
+    if (args->philo.index % 2 == 0)
+    {
+        while (1)
+        {
+            pthread_mutex_lock(&args->vars->mutex);
+            if (args->philo.index == 1)
+            {
+                if (args->vars->forks[0].status 
+                    || args->vars->forks[args->vars->nbr_philo - 1].status)
+                    usleep(args->vars->t_die);
+                if (args->vars->forks[0].status 
+                    || args->vars->forks[args->vars->nbr_philo - 1].status)
+                {
+                    printf("philo die !!\n");
+                    exit(1);
+                }
+                args->vars->forks[0].status = 1;
+                args->vars->forks[args->vars->nbr_philo - 1].status = 1;
+            }
+            else
+            {
+                if (args->vars->forks[args->philo.index - 1].status 
+                    || args->vars->forks[args->philo.index - 2].status)
+                    usleep(args->vars->t_die);
+                if (args->vars->forks[args->philo.index - 1].status 
+                    || args->vars->forks[args->philo.index - 2].status)
+                {
+                    printf("philo die !!\n");
+                    exit(1);
+                }
+                args->vars->forks[args->philo.index - 1].status = 1;
+                args->vars->forks[args->philo.index - 2].status = 1;
+            }
+            pthread_mutex_unlock(&args->vars->mutex);
+            printf("philo N %d start eating\n", args->philo.index);
+            usleep(args->vars->t_eat);
+            pthread_mutex_lock(&args->vars->mutex);
+            if (args->philo.index == 1)
+            {
+                args->vars->forks[0].status = 0;
+                args->vars->forks[args->vars->nbr_philo - 1].status = 0;
+            }
+            else
+            {
+                args->vars->forks[args->philo.index - 1].status = 0;
+                args->vars->forks[args->philo.index - 2].status = 0;
+            }
+            pthread_mutex_unlock(&args->vars->mutex);
+            printf("philo N %d start sleaping\n", args->philo.index);
+            usleep(args->vars->t_eat);
+            printf("philo N %d start thinking\n", args->philo.index);
+        }
+    }
+    else
+    {
+        while (1)
+        {
+        printf("philo N %d start sleeping\n", args->philo.index);
+        usleep(args->vars->t_sleep);
+        pthread_mutex_lock(&args->vars->mutex);
+        if (args->philo.index == 1)
+        {
+            if (args->vars->forks[0].status 
+                || args->vars->forks[args->vars->nbr_philo - 1].status)
+                usleep(args->vars->t_die);
+            if (args->vars->forks[0].status 
+                || args->vars->forks[args->vars->nbr_philo - 1].status)
+            {
+                printf("philo die !!\n");
+                exit(1);
+            }
+            args->vars->forks[0].status = 1;
+            args->vars->forks[args->vars->nbr_philo - 1].status = 1;
+        }
+        else
+        {
+            if (args->vars->forks[args->philo.index - 1].status 
+                || args->vars->forks[args->philo.index - 2].status)
+                usleep(args->vars->t_die);
+            if (args->vars->forks[args->philo.index - 1].status 
+                || args->vars->forks[args->philo.index - 2].status)
+            {
+                printf("philo die !!\n");
+                exit(1);
+            }
+            args->vars->forks[args->philo.index - 1].status = 1;
+            args->vars->forks[args->philo.index - 2].status = 1;
+        }
+        pthread_mutex_unlock(&args->vars->mutex);
+        printf("philo N %d start eating\n", args->philo.index);
+        usleep(args->vars->t_eat);
+        pthread_mutex_lock(&args->vars->mutex);
+        if (args->philo.index == 1)
+        {
+            args->vars->forks[0].status = 0;
+            args->vars->forks[args->vars->nbr_philo - 1].status = 0;
+        }
+        else
+        {
+            args->vars->forks[args->philo.index - 1].status = 0;
+            args->vars->forks[args->philo.index - 2].status = 0;
+        }
+        pthread_mutex_unlock(&args->vars->mutex);
+        printf("philo N %d start thinking\n", args->philo.index);
+        }
+    }
     
     return (NULL);
 }
@@ -60,6 +161,9 @@ void    philo_loop(int nbr_philo)
     t_vars  vars;
     
     vars.nbr_philo = nbr_philo;
+    vars.t_die = 100;
+    vars.t_eat = 100;
+    vars.t_sleep = 100;
     ft_philos_init(&vars);
     ft_philos_join(&vars);
 }
@@ -69,6 +173,6 @@ int main(int ac, char *av[])
     (void)ac;
     (void)av;
     
-    philo_loop(4);
+    philo_loop(6);
     return (0);
 }
