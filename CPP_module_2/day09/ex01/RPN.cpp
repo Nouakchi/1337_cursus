59f206fa2a5f1cf06b8efa666c11edeceb9ab3e7
othman <othman@halal007.shmaliger>
2023-10-05 14:06:37 +0100

    add aditional updates on parsing

--- CPP_module_2/day09/ex01/RPN.cpp
+++ CPP_module_2/day09/ex01/RPN.cpp
@@ -10,12 +10,43 @@ RPN::RPN( const RPN& copy ) { (void) copy; }
 
 RPN &RPN::operator= ( const RPN& other ) { (void) other; return (*this); }
 
-void    RPN::setSequence( std::string seq )
+int     RPN::parseLine( std::string seq )
 {
-    (void)seq;
+    int i;
+    int number_is_taken;
+    int op_is_taken;
+
+    i = -1;
+    number_is_taken = 0;
+    op_is_taken = 0;
+    while (seq[++i])
+    {
+        if (seq[i] == ' ')
+        {
+            number_is_taken = 0;
+            continue;
+        }
+        if (seq[i] < '0' || seq[i] > '9')
+        {
+            if (!strchr("+-/*", seq[i]))
+                return (1);
+            else if (op_is_taken)
+                return (1);
+            op_is_taken = 1;
+            number_is_taken = 0;
+        }
+        else
+        {
+            if (number_is_taken)
+                return (1);
+            number_is_taken = 1;
+            op_is_taken = 0;
+        }
+    }
+    return (0);
 }
 
-int     RPN::parseLine( std::string seq )
+void    RPN::setSequence( std::string seq )
 {
     int i;
 
@@ -24,15 +55,8 @@ int     RPN::parseLine( std::string seq )
     {
         if (seq[i] == ' ')
             continue;
-        if (seq[i] < '0' || seq[i] > '9')
-        {
-            if (!strchr("+-/*", seq[i]))
-                return (1);
-            if (i && strchr("+-/*", seq[i - 1]))
-                return (1);
-        }
+        this->sequence.push_back(seq[i]);
     }
-    return (0);
 }
 
 int RPN::calculate()
