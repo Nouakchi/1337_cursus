4f01b13d791f9f9a095924aacb6cccc1c5b4e4a4
othman <othman@halal007.shmaliger>
2023-10-06 18:03:00 +0100

    starting ford jhonson merge-insert sorting algo

---
+++ CPP_module_2/day09/ex02/PmergeMe.hpp
@@ -0,0 +1,15 @@
+
+# ifndef __PMERGEME_H__
+# define _PMERGEME_H__
+
+# include <iostream>
+
+class PmergeMe
+{
+    private:
+
+    public:
+        
+}
+
+# endif
---
+++ CPP_module_2/day09/ex02/main.cpp
@@ -0,0 +1,8 @@
+
+# include "PmergeMe.hpp"
+
+int main(int ac, char *av[])
+{
+
+    return (0);
+}
